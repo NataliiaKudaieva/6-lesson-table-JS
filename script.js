913ccd60648ca6c861c860dc6e655cb6c1a5c2db
NataliiaKudaieva <81326193+NataliiaKudaieva@users.noreply.github.com>
2021-08-18 00:23:37 +0300

    JS multiplication

---
+++ script.js
@@ -0,0 +1,9 @@
+    for (var i = 1; i <= 10; i++) { 
+         for (var n = 1; n <= 10; n++) {
+             multiplication = i*n;
+             console.log(multiplication)
+             if(n == 10) {
+                console.log('/--/--/--/--/--/');
+            }
+         }
+      }
